1d4b0dc88e29d7deeee10c7759cc30f954187a1a
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-08-05 12:54:08 +0000

    Add style to the navigation bar

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-once': 'bounce 2s 1'
+      },
+    },
   },
   variants: {
     extend: {},
