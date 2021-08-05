6cc0596982eb1d940b97819f7b60d6d3ca6e19ad
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-08-05 08:05:57 +0000

    Add blog listing page model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class BlogIndexPage(Page):
+    """
+        Page to list all blog posts
+    """
+
+    max_count = 1
+    template = "blog/blog-list.html"
+    parent_page_types = ["home.HomePage"]
+
+    intro = RichTextField("Intro Text")
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full")
+    ]
