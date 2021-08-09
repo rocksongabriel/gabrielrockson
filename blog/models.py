5cf8bf26c3c835335be800e9d8169637a5608a85
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-08-09 06:11:29 +0000

    Add The BlogAuthor snippet model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,64 @@
 from django.db import models
+from django.db.models.fields import Field
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from django.utils.translation import gettext_lazy as _
+from wagtail.snippets.models import register_snippet
+
+
+# Snippets
+@register_snippet
+class BlogAuthor(models.Model):
+    
+    """
+        - full name
+        - avatar 
+        - description
+        - date joined
+        - twitter
+        - email
+    """
+    full_name = models.CharField(_("Full Name"), max_length=50, null=False, blank=False, help_text="Enter the full name of the author")
+    avatar = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        help_text="Upload an image of the author",
+        related_name="+"
+    )
+    description = RichTextField(_("Description of Author"), help_text="Enter a brief description about the author")
+    twitter = models.URLField("Twitter Handle", help_text="Input the the link to the author's twitter handle", null=False, blank=False)
+    email = models.EmailField("Email Address", help_text="Enter the email address of the author", max_length=500)
+
+
+    panels = [
+        FieldPanel("full_name"),
+        ImageChooserPanel("avatar"),
+        FieldPanel("description"),
+        FieldRowPanel([
+            FieldPanel("twitter"),
+            FieldPanel("email")
+        ])
+    ]
+
+    def __str__(self):
+        return self.full_name
+
+
+    class Meta:
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
+
+
+# class BlogCategory(models.Model):
+    
+#     class Meta:
+#         verbose_name = "Blog Category"
+#         verbose_name_plural = "Blog Categories"
 
 
 class BlogIndexPage(Page):
@@ -13,6 +69,7 @@ class BlogIndexPage(Page):
     max_count = 1
     template = "blog/blog-list.html"
     parent_page_types = ["home.HomePage"]
+    subpages_types = ["blog.BlogDetailPage"]
 
     intro = RichTextField("Intro Text")
 
@@ -20,3 +77,46 @@ class BlogIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro", classname="full")
     ]
+
+
+# class BlogDetailPage(Page):
+
+#     template = "blog/blog-detail.html"
+#     parent_page_types = ["blog.BlogIndexPage"]
+
+#     """
+#         - blog title
+#         - featured_image
+#         - author *
+#         - date published
+#         - upvotes *
+#         - times read *
+#         - read time *
+#         - blog category
+#         - blog tags
+#         - programming quote
+#         - main content
+#             - titles
+#             - paragraphs
+#             - code blocks
+#             - images
+
+
+#     """
+#     image = models.ForeignKey(
+#         _("Featured Image"),
+#         "wagtailimages.Image",
+#         null=False,
+#         blank=False,
+#         related_name="+",
+#         help_text="Select an image to use on the post's page"
+#     )
+#     date_published = models.DateTimeField(auto_now_add=True)
+#     quote = models.TextField(_("A Programming Quote"), help_text="Enter some programming quote", blank=False, null=False)
+
+
+
+#     content_panels = Page.content_panels + [
+#         ImageChooserPanel("image"),
+#         FieldPanel("quote"),
+#     ]
