aa897bbb9323e2ab3b5d82582e52a3465b72b512
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-08-09 06:21:02 +0000

    Merge with blog

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      animation: {
-        'bounce-once': 'bounce 2s 1'
+      colors: {
+        'imperial-red': '#e63946',
+        honeydew: '#f1faee',
+        'rich-black': '#050609',
+        'caledon-blue': '#457b9d',
+        'prussian-blue': '#1d3557',
       },
+      fontFamily: {
+        cardo: ['Cardo', 'serif'],
+        cormorant: ['Cormorant', 'serif'],
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif'],
+        arvo: ['Arvo', 'serif']
+      }
     },
   },
   variants: {
