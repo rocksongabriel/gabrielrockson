2be3bc46e9c2a4a17f91b7e384ddd71a87773d76
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-08-10 02:38:42 +0000

    Add the content field to the blog detail page

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,15 @@ from django.utils.translation import gettext_lazy as _
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey
 from taggit.models import TaggedItemBase
-from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel, MultiFieldPanel
-from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
+                                         MultiFieldPanel, StreamFieldPanel)
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.images.blocks import ImageChooserBlock
 
 
 # ------------------------------------------------------------- SNIPPETS ----------------------------------------------------------------
@@ -149,6 +152,12 @@ class BlogDetailPage(Page):
         help_text="Select the category of this blog post"
     )
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    content = StreamField([
+        ("heading", blocks.CharBlock()),
+        ("text", blocks.TextBlock()),
+        ("paragraph", blocks.RichTextBlock()),
+        ("image", ImageChooserBlock()),
+    ])
 
 
     content_panels = Page.content_panels + [
@@ -159,4 +168,5 @@ class BlogDetailPage(Page):
         ]),
         FieldPanel("tags"),
         FieldPanel("quote"),
+        StreamFieldPanel("content"),
     ]
